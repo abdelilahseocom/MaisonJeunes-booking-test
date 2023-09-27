72234ecf3318e4e49951c21dc12c5bdb7b876bda
souhayl seocom <souhayl.seocom@gmail.com>
2023-09-27 11:29:37 +0100

    Change default langugae to french + add translated strings

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'failed'   => "Les informations d'identification ne correspondent pas à nos enregistrements.",
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :secondes seconds.',
+];
